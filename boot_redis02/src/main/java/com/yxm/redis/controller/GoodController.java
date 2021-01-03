284605f5bab493493be6488e2cb7099086004c1f
yxm252413 <1174198674@qq.com>
2021-01-03 18:06:57 +0800

    4.0版使用setnx加锁升级： finally {//无论如何都会执行的代码块，如果上面的代码出现异常，redis锁也可以正常释放             //程序执行完毕，删除redis锁             stringRedisTemplate.delete(REDIS_LOCK);         }

--- boot_redis02/src/main/java/com/yxm/redis/controller/GoodController.java
+++ boot_redis02/src/main/java/com/yxm/redis/controller/GoodController.java
@@ -33,24 +33,27 @@ public class GoodController {
     public String buy_Goods() {
         //redis锁的value
         String value = UUID.randomUUID().toString() + Thread.currentThread().getName();
-        //加锁
-        boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK, value).booleanValue();//相当于redis的setNX
-        String result = stringRedisTemplate.opsForValue().get("goods:001");
-        int goodsNumber = result == null ? 0 : Integer.parseInt(result);
+        try {
+            //加锁
+            boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK, value).booleanValue();//相当于redis的setNX
+            String result = stringRedisTemplate.opsForValue().get("goods:001");
+            int goodsNumber = result == null ? 0 : Integer.parseInt(result);
 
-        if (!flag) {
-            return "抢锁失败！！！";
-        }
-        if (goodsNumber > 0) {
-            int realNumber = goodsNumber - 1;
-            stringRedisTemplate.opsForValue().set("goods:001", realNumber + "");
-            System.out.println("你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort);
+            if (!flag) {
+                return "抢锁失败！！！";
+            }
+            if (goodsNumber > 0) {
+                int realNumber = goodsNumber - 1;
+                stringRedisTemplate.opsForValue().set("goods:001", realNumber + "");
+                System.out.println("你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort);
+                return "你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort;
+            } else {
+                System.out.println("商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort);
+            }
+            return "商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort;
+        } finally {//无论如何都会执行的代码块，如果上面的代码出现异常，redis锁也可以正常释放
             //程序执行完毕，删除redis锁
             stringRedisTemplate.delete(REDIS_LOCK);
-            return "你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort;
-        } else {
-            System.out.println("商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort);
         }
-        return "商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort;
     }
 }
